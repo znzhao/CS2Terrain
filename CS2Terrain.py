a9844249776829deef0cd5aad09d56193dc38da6
znzhao <46983185+znzhao@users.noreply.github.com>
2024-04-16 23:24:44 -0500

    Version 0.11
    - Add Terrain Map and World Map
    - Add submit button
    - Fix progress bar issue
    - Fine tuned params

--- CS2Terrain.py
+++ CS2Terrain.py
@@ -45,7 +45,7 @@ with expand1:
     noise_size = st.slider('Noise Terrain Weight' if not CHN else "噪声地形权重", min_value = 0.0, max_value = 1.0, value=0.5)
     dim =  st.selectbox('Resize Dimension' if not CHN else "缩放精度", index = 1, options=[256, 512, 1024])
     if dim == 1024: st.warning('Warning: a large dimension might slow down the speed!'  if not CHN else "警告：缩放精度过高会导致运行速度降低，请谨慎选择！")
-    ks2 = st.selectbox('Reverse Kernel Size' if not CHN else "复原平滑卷积核", index = 0, options=[4, 8, 16, 32, 64, 128])
+    ks2 = st.selectbox('Reverse Kernel Size' if not CHN else "复原平滑卷积核", index = 4, options=[4, 8, 16, 32, 64, 128, 256, 512,])
     revise_kernel = (ks2-1, ks2-1)
     min_altitude, max_altitude = st.slider('Output Height Range(meter) (Baseline Plain = 500m)' if not CHN else "输出高度范围（米）(基准平原为500米)", 0, 4000, (300, 1200))
 
@@ -113,6 +113,18 @@ with expand3:
     repose_slope = st.number_input('Repose Slope' if not CHN else "休止坡度", value=0.03, min_value=0.0, disabled=not erosion)
     gravity = st.number_input('Gravity' if not CHN else "重力", value=30.0, min_value=0.0, disabled=not erosion)
 
+if 'first_submitted' not in st.session_state:
+    st.session_state.first_submitted = True
+
+if 'submitted' not in st.session_state:
+    st.session_state.submitted = False
+    
+
+def onclick_submit():
+    st.session_state.submitted = True
+
+st.sidebar.button("Submit" if not CHN else "提交", on_click=onclick_submit)
+
 if 'input_height' not in st.session_state:
     st.session_state.input_height = np.ones(shape=(dim, dim))*plainx/100
 
@@ -135,44 +147,80 @@ if 'output_height' not in st.session_state:
 
 progress_text = 'Erosion in progress. Please wait.' if not CHN else '侵蚀模型处理中，请稍后...'
 progress_text2 = 'Erosion finished.' if not CHN else '侵蚀模型处理完成。'
+
 def click_submit():
     st.session_state.output_height = processTerrain(sorted_intplx, sorted_loudness, dim, seed, ksize, init_size, noise_size, erosion, full_width, rain_rate, 
                                                     evaporation_rate, min_height_delta, repose_slope, gravity, min_altitude, max_altitude, 
                                                     input_height = st.session_state.input_height, progress_text = progress_text, progress_text2 = progress_text2)
 
-if uploaded_file is not None:
-    # Get actual image file
-    bytes_data = get_image_path(uploaded_file)
-    # ReSize
-    input_height = cv2.resize(cv2.imread(bytes_data, cv2.IMREAD_ANYDEPTH), (dim, dim))/65536
-    st.session_state.input_height = input_height
-else:
-    st.session_state.input_height = np.ones(shape=(dim, dim))*plainx/100
+if st.session_state.submitted or st.session_state.first_submitted:
+    if uploaded_file is not None:
+        # Get actual image file
+        bytes_data = get_image_path(uploaded_file)
+        # ReSize
+        input_height = cv2.resize(cv2.imread(bytes_data, cv2.IMREAD_ANYDEPTH), (dim, dim))/65536
+        st.session_state.input_height = input_height
+    else:
+        st.session_state.input_height = np.ones(shape=(dim, dim))*plainx/100
      
-if st.session_state.input_height is not None:
-    color_continuous_scale=[(0,'rgb(23, 23, 121)'), (0.25, 'rgb(0, 166, 80)'), (0.5,'rgb(216, 218, 120)'), (0.75,'rgb(98, 64, 57)'), (1,'rgb(255, 255, 255)')]
-    input_map = go.Figure(data=go.Contour(z=st.session_state.input_height*4000, colorscale=color_continuous_scale)) if not threeD else go.Figure(data=[go.Surface(z=st.session_state.input_height*4000, colorscale=color_continuous_scale)])
-    input_map.update_layout(height = 700, title_text='Input' if not CHN else "输入高度图", title_x=0.5, title_xanchor= 'center')
-    colm1.plotly_chart(input_map, use_container_width=True)
+if st.session_state.input_height is not None and (st.session_state.submitted or st.session_state.first_submitted):
     click_submit()
 
+if st.session_state.input_height is not None:
+    color_continuous_scale=[(0,'rgb(23, 23, 121)'), (0.25, 'rgb(0, 166, 80)'), (0.5,'rgb(216, 218, 120)'), (0.75,'rgb(98, 64, 57)'), (1,'rgb(255, 255, 255)')]
+    if not threeD:
+        input_map = go.Figure(data=go.Contour(z=st.session_state.input_height*4000, colorscale=color_continuous_scale))
+        input_map.add_hline(y=0.375*dim, line_width=3, line_dash="dash", line_color="black")
+        input_map.add_hline(y=(1-0.375)*dim, line_width=3, line_dash="dash", line_color="black")
+        input_map.add_vline(x=0.375*dim, line_width=3, line_dash="dash", line_color="black")
+        input_map.add_vline(x=(1-0.375)*dim, line_width=3, line_dash="dash", line_color="black")
+    else:
+        input_map = go.Figure(data=[go.Surface(z=st.session_state.input_height*4000, colorscale=color_continuous_scale)])
+    input_map.update_layout(height = 700, title_text='Input' if not CHN else "输入高度图", title_x=0.5, title_xanchor= 'center')
+    colm1.plotly_chart(input_map, use_container_width=True)
+
 if st.session_state.output_height is not None:
-    output_map = go.Figure(data=go.Contour(z=st.session_state.output_height*4000, colorscale=color_continuous_scale)) if not threeD else go.Figure(data=[go.Surface(z=st.session_state.output_height, colorscale=color_continuous_scale)])
+    color_continuous_scale=[(0,'rgb(23, 23, 121)'), (0.25, 'rgb(0, 166, 80)'), (0.5,'rgb(216, 218, 120)'), (0.75,'rgb(98, 64, 57)'), (1,'rgb(255, 255, 255)')]
+    if not threeD:
+        output_map = go.Figure(data=go.Contour(z=cv2.resize(st.session_state.output_height,(512,512))*4000, colorscale=color_continuous_scale))
+        output_map.add_hline(y=0.375*512, line_width=3, line_dash="dash", line_color="black")
+        output_map.add_hline(y=(1-0.375)*512, line_width=3, line_dash="dash", line_color="black")
+        output_map.add_vline(x=0.375*512, line_width=3, line_dash="dash", line_color="black")
+        output_map.add_vline(x=(1-0.375)*512, line_width=3, line_dash="dash", line_color="black")
+    else:
+        output_map = go.Figure(data=[go.Surface(z=cv2.resize(st.session_state.output_height,(512,512))*4000, colorscale=color_continuous_scale)])
     output_map.update_layout(height = 700, title_text='Output' if not CHN else "输出高度图", title_x=0.5, title_xanchor= 'center')
     colm2.plotly_chart(output_map, use_container_width=True)
 
-    terrain = cv2.resize(st.session_state.output_height, (4096, 4096), interpolation=cv2.INTER_AREA)*65536
-    terrain = cv2.GaussianBlur(terrain, revise_kernel, 0)
+    worldmap = cv2.resize(st.session_state.output_height, (4096, 4096), interpolation=cv2.INTER_AREA)*65536
+    worldmap = cv2.GaussianBlur(worldmap, revise_kernel, 0)
+    worldmap16 = worldmap.astype(np.uint16)
+    is_success, bufferWM = cv2.imencode(".png", worldmap16)
+    io_buf_WM = io.BytesIO(bufferWM)
+
+    _, _, _, col1 = st.columns(4, gap = 'small')
+    _, _, _, col2 = st.columns(4, gap = 'small')
+    with io_buf_WM as fileWM:
+        col1.download_button(label="Download World Map" if not CHN else "下载世界地图", data = fileWM, file_name='World Map.png', mime="image/png",type = 'primary', use_container_width = True)
+
+    terrain = cv2.resize(worldmap[1536:-1536, 1536:-1536], (4096, 4096), interpolation=cv2.INTER_CUBIC)
+    terrain = cv2.GaussianBlur(terrain, revise_kernel/2, 0)
     terrain = terrain.astype(np.uint16)
     is_success, buffer = cv2.imencode(".png", terrain)
     io_buf = io.BytesIO(buffer)
+
     with io_buf as file:
-        st.download_button(label="Download" if not CHN else "下载地形图", data = file, file_name='Output.png', mime="image/png",type = 'primary')
+        col2.download_button(label="Download Terrain Height Map" if not CHN else "下载地形高度图", data = file, file_name='Terrain.png', mime="image/png",type = 'primary', use_container_width = True)
+
 
 st.divider()
 footer = '''
+For best performance, set resize dimension to 512, set Reverse Kernel Size to 64, and turn on the rain erosion.
+
 Developed by Zhenning Zhao. Contact the Author at [znzhaopersonal@gmail.com](mailto://znzhaopersonal@gmail.com). See tech document [here](https://github.com/znzhao/CS2Terrain).
 ''' if not CHN else '''
+推荐参数：为保证最佳画面表现力，请使用512缩放精度，64位复原平滑卷积核，并打开雨水侵蚀功能。
+
 本地形编辑器由作者调用开源代码制作，无偿提供，不对有效性及后续技术支持提供保证，请谨慎使用。
 
 [点此处](mailto://znzhaopersonal@gmail.com)联系开发者。B站主页：[@红色的青蛙](https://space.bilibili.com/124723924?spm_id_from=333.788.0.0)
@@ -180,3 +228,6 @@ Developed by Zhenning Zhao. Contact the Author at [znzhaopersonal@gmail.com](mai
 详细技术文档请查阅[此文件](https://github.com/znzhao/CS2Terrain)。
 '''
 st.markdown(footer,unsafe_allow_html=True)
+
+st.session_state.submitted = False
+st.session_state.first_submitted = False
